28cd607e828646dd0ff40202baec52af593d78fd
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2021-12-03 12:35:52 -0800

    added to admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,53 @@
+async function main() {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+    books.forEach(renderBook)
+}
 
-// Your Code Here
+function renderBook(book) {
+    let root = document.querySelector('#root')
+
+    let li = document.createElement("li")
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    //Save button// 
+    let saveBttn = document.createElement('button')
+    saveBttn.textContent = 'Save'
+    
+    saveBttn.addEventListener('click', function() {
+        fetch('http://localhost:3001/updateBook'), {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        }
+
+    })
+
+
+    //Delete Button//
+    let deleteBttn = document.createElement('button')
+    deleteBttn.textContent = 'Delete'
+
+    deleteBttn.addEventListener('click', function () {
+        fetch('http://localhost:3001/removeBook/{bookId}'), {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    })
+    
+    root.append(li)
+    root.append(saveBttn)
+    root.append(quantityInput)
+}
+ 
+main();
